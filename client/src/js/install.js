dc2000aeda153d6e225027e5c2828eb50ea52ae1
Ben Merchant <benmerch@gmail.com>
2024-09-01 20:42:16 -0500

    Hiding the nav button instead of just the button.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,4 +1,5 @@
 const btnInstall = document.getElementById("buttonInstall");
+const navBtnInstall = document.getElementById("nav-install-btn");
 
 /* Here we need to provide the logic for when and how to install the PWA.  */
 
@@ -6,6 +7,7 @@ window.addEventListener("beforeinstallprompt", (event) => {
   event.preventDefault();
   window.deferredPrompt = event;
   btnInstall.classList.remove("hidden");
+  navBtnInstall.classList.remove("hidden");
 });
 
 // This listener fires when the user clicks the "install" button.
@@ -32,10 +34,12 @@ btnInstall.addEventListener("click", async () => {
 
   // Hide the install button
   btnInstall.classList.add("hidden");
+  navBtnInstall.classList.add("hidden");
 });
 
 window.addEventListener("appinstalled", (event) => {
   console.log("PWA was installed.");
   window.deferredPrompt = null;
   btnInstall.classList.add("hidden");
+  navBtnInstall.classList.add("hidden");
 });
